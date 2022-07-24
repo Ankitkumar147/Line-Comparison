047170972808b9fd14d3f3cf3f336e8eebc71133
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-07-24 22:50:10 +0530

    Added :- Line Equality Logic

--- LineComparison/src/com/Bridgelabz/RFP/LineComparison.java
+++ LineComparison/src/com/Bridgelabz/RFP/LineComparison.java
@@ -1,11 +1,13 @@
 package com.Bridgelabz.RFP;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import java.util.Scanner;
 
 public class LineComparison {
 
     public static void main(String[] args) {
-        //float x1,x2,y1,y2;
+        //float x1,x2,y1,y2,a1,b1,a2,b2;
         System.out.println("Welcome to the Line Comparison Computation Program");
         System.out.println("Enter the line point according to cartesian System as (x1,y1) & (x2,y2) ");
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +19,24 @@ public class LineComparison {
         float x2 = scanner.nextFloat();
         System.out.println("Enter the value of point y2 :-  ");
         float y2 = scanner.nextFloat();
+        System.out.println("Enter the Value of Points a1 :- ");
+        float a1 = scanner.nextFloat();
+        System.out.println("Enter the Value of Points b1 :- ");
+        float b1 = scanner.nextFloat();
+        System.out.println("Enter the Value of Points a2 :- ");
+        float a2 = scanner.nextFloat();
+        System.out.println("Enter the Value of Points b2 :- ");
+        float b2 = scanner.nextFloat();
         System.out.println("You have entered"+ "("+x1+","+y1+") & ("+x2+","+y2+")");
+        System.out.println("You have entered"+ "("+a1+","+b1+") & ("+a2+","+b2+")");
+        float firstLineLength =(float)Math.sqrt(Math.pow(x2-x1, 2) + (Math.pow(y2-y1, 2)));
+        float secondLineLength =(float)Math.sqrt(Math.pow(a2-a1, 2) + (Math.pow(b2-b1, 2)));
+        System.out.println("First Line Length :- " +firstLineLength);
+        System.out.println("Second Line Length :- " +secondLineLength);
+        if (firstLineLength==secondLineLength)
+        System.out.println("Both lines are Equal");
+        else
+        System.out.println("Lines are not Equal");
     }
 }
 
