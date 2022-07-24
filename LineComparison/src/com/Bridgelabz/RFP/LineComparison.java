30ab1e4ff973c7f5942e336a73aed3757363aa3a
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-07-24 23:26:24 +0530

    Added :- Comapre to logic for comparing lines.

--- LineComparison/src/com/Bridgelabz/RFP/LineComparison.java
+++ LineComparison/src/com/Bridgelabz/RFP/LineComparison.java
@@ -1,13 +1,10 @@
 package com.Bridgelabz.RFP;
 
-import com.sun.deploy.security.SelectableSecurityManager;
-
 import java.util.Scanner;
 
 public class LineComparison {
 
     public static void main(String[] args) {
-        //float x1,x2,y1,y2,a1,b1,a2,b2;
         System.out.println("Welcome to the Line Comparison Computation Program");
         System.out.println("Enter the line point according to cartesian System as (x1,y1) & (x2,y2) ");
         Scanner scanner = new Scanner(System.in);
@@ -27,16 +24,20 @@ public class LineComparison {
         float a2 = scanner.nextFloat();
         System.out.println("Enter the Value of Points b2 :- ");
         float b2 = scanner.nextFloat();
-        System.out.println("You have entered"+ "("+x1+","+y1+") & ("+x2+","+y2+")");
-        System.out.println("You have entered"+ "("+a1+","+b1+") & ("+a2+","+b2+")");
-        float firstLineLength =(float)Math.sqrt(Math.pow(x2-x1, 2) + (Math.pow(y2-y1, 2)));
-        float secondLineLength =(float)Math.sqrt(Math.pow(a2-a1, 2) + (Math.pow(b2-b1, 2)));
-        System.out.println("First Line Length :- " +firstLineLength);
-        System.out.println("Second Line Length :- " +secondLineLength);
-        if (firstLineLength==secondLineLength)
+        System.out.println("You have entered" + "(" + x1 + "," + y1 + ") & (" + x2 + "," + y2 + ")");
+        System.out.println("You have entered" + "(" + a1 + "," + b1 + ") & (" + a2 + "," + b2 + ")");
+        float firstLineLength = (float) Math.sqrt(Math.pow(x2 - x1, 2) + (Math.pow(y2 - y1, 2)));
+        float secondLineLength = (float) Math.sqrt(Math.pow(a2 - a1, 2) + (Math.pow(b2 - b1, 2)));
+        System.out.println("First Line Length :- " + firstLineLength);
+        System.out.println("Second Line Length :- " + secondLineLength);
+        int compareValue = Float.compare(firstLineLength , secondLineLength);
+        if (compareValue == 0) {
             System.out.println("Both lines are Equal");
-        else
-            System.out.println("Lines are not Equal");
+        } else if (compareValue > 0) {
+            System.out.println("First Lines is greater than second line");
+        } else {
+            System.out.println("Second Line greater than First Line");
+        }
     }
 }
 
